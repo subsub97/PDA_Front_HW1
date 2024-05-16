ee0a1835cccf0ba6a22f07610c432f28d6ae2e07
Ho-Da-Dak <godqhr721@gmail.com>
2024-05-16 19:29:36 +0900

    feature : stocks.js 삼성전자 주식 차트 주봉 JSON 받아오기 기능 추가

--- homework-node/src/stock.js
+++ homework-node/src/stock.js
@@ -5,7 +5,29 @@ import fs from 'fs'
 json에 들어갈 필수 key
 [date, tradePrice(종가), openingPrice, highPrice, lowPrice, candleAccTradePrice(거래대금)]
 */
-async function main(){}
+
+const userAgentValue = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
+
+const payload = {
+  limit: 200,
+  adjusted: true
+}
+
+async function main(){
+  const url = `https://finance.daum.net/api/charts/A005930/weeks?limit=200&adjusted=true`;
+
+  try{
+    const response = await axios.get(url,{
+                headers: {
+                'User-Agent': userAgentValue
+                }}
+    );
+    console.log(response.data);
+  }
+  catch (error) {
+    console.error("차트 정보 받아오기 실패...", error);
+  }
+}
 
 
 main();
