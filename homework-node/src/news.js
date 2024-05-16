0cfef2f71e90ad27a4d3bcdefa71f314803db636
Ho-Da-Dak <godqhr721@gmail.com>
2024-05-16 19:03:59 +0900

    일자별 JSON 저장 기능 추가

--- homework-node/src/news.js
+++ homework-node/src/news.js
@@ -10,48 +10,63 @@ import fs from 'fs'
     - (20240415 00시 00분 00초~20240419 23시59분59초)
     - 저장 형태는 Object형태로 하시되 key는 끝나는 날짜, value는 array<object({title:기사제목, url: 기사링크})>로 저장하기
 */
-let userAgentValue = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
+const userAgentValue = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
+const searchDates = [
+    ["20240401000000", "20240405235959"],
+    ["20240408000000", "20240412235959"],
+    ["20240415000000", "20240419235959"]
+]
 
 async function getNewsInfo() {
-    //아래 기간 변수처리하기 
-    const url = `https://search.daum.net/search?w=news&nil_search=btn&DA=STC&enc=utf8&cluster=y&cluster_page=1&q=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sd=20240401000000&ed=20240405235959&period=u`
+    for(let i = 0; i < 3; i++) {
 
-    try { 
-        const articles = [];
+    
+        const url = `https://search.daum.net/search?w=news&nil_search=btn&DA=STC&enc=utf8&cluster=y&cluster_page=1&q=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sd=${searchDates[0]}&ed=${searchDates[1]}&period=u`
 
-        const response = await axios.get(url, {
-            headers: {
-            'User-Agent': userAgentValue
-            }
-        });
+        try { 
+            const articles = [];
 
-        const data = await response.data;
-        const $ = cheerio.load(data);
-
-        $('.c-list-basic li').each((i, e) => {
-            const title = $(e).find('.item-title').text().trim();
-            if(title === "") return;
-            const articleUrl = $(e).find('.conts-desc > a').prop('href');
-            
-            
-            articles.push({
-                title : title,
-                url : articleUrl
+            const response = await axios.get(url, {
+                headers: {
+                'User-Agent': userAgentValue
+                }
             });
-        })
 
-        await saveNewsToJson(articles)
-        
-    }
-    catch (error) {
-        console.error("기사 가져오기 실패.", error);
+            const data = await response.data;
+            const $ = cheerio.load(data);
+
+            $('.c-list-basic li').each((i, e) => {
+                const title = $(e).find('.item-title').text().trim();
+                if(title === "") return;
+                const articleUrl = $(e).find('.conts-desc > a').prop('href');
+                
+                
+                articles.push({
+                    title : title,
+                    url : articleUrl
+                });
+            })
+
+            await saveNewsToJson(articles,`${searchDates[i][1].slice(0,8)}`)
+            
+        }
+        catch (error) {
+            console.error("기사 가져오기 실패.", error);
+        }
     }
 }
 
-async function saveNewsToJson(news) {
+async function saveNewsToJson(news,date) {
   try {
-    const jsonData = JSON.stringify(news, null, 2);
-    await fs.promises.writeFile(`news_page.json`, jsonData, 'utf8');
+    // 날짜 별 key 설정
+    const formattedNews = {
+        [date]: news.map(item => ({
+            title: item.title,
+            url: item.url
+        }))
+    }
+    const jsonData = JSON.stringify(formattedNews, null, 2);
+    await fs.promises.writeFile(`news.json`, jsonData, 'utf8');
   } catch (err) {
     console.error("파일 저장 실패", err);
   }
