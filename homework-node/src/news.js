7fee4cfbc4dbbbccb2a3fcaf270cc12759d6c841
Ho-Da-Dak <godqhr721@gmail.com>
2024-05-16 18:47:58 +0900

    feature : news.js JSON 파일 저장 기능 추가

--- homework-node/src/news.js
+++ homework-node/src/news.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import { generateKey } from 'crypto';
 import fs from 'fs'
 
 /**
@@ -9,19 +10,54 @@ import fs from 'fs'
     - (20240415 00시 00분 00초~20240419 23시59분59초)
     - 저장 형태는 Object형태로 하시되 key는 끝나는 날짜, value는 array<object({title:기사제목, url: 기사링크})>로 저장하기
 */
+let userAgentValue = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
 
-//아래 기간 변수처리하기 
-const url = `https://search.daum.net/search?w=news&cluster=y&q=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sd=20240401000000&ed=20240405235959&period=u&DA=STC`
+async function getNewsInfo() {
+    //아래 기간 변수처리하기 
+    const url = `https://search.daum.net/search?w=news&nil_search=btn&DA=STC&enc=utf8&cluster=y&cluster_page=1&q=%EC%82%BC%EC%84%B1%EC%A0%84%EC%9E%90&sd=20240401000000&ed=20240405235959&period=u`
 
-try {
-    const response = await axios.get(url);
-    const data = response.data;
-    const $ = cheerio.load(data);
+    try { 
+        const articles = [];
 
-    console.log($.html());
+        const response = await axios.get(url, {
+            headers: {
+            'User-Agent': userAgentValue
+            }
+        });
+
+        const data = await response.data;
+        const $ = cheerio.load(data);
+
+        $('.c-list-basic li').each((i, e) => {
+            const title = $(e).find('.item-title').text().trim();
+            if(title === "") return;
+            const articleUrl = $(e).find('.conts-desc > a').prop('href');
+            
+            
+            articles.push({
+                title : title,
+                url : articleUrl
+            });
+        })
+
+        await saveNewsToJson(articles)
+        
+    }
+    catch (error) {
+        console.error("기사 가져오기 실패.", error);
+    }
 }
-catch (error) {
-    console.error("기사 가져오기 실패.", error);
+
+async function saveNewsToJson(news) {
+  try {
+    const jsonData = JSON.stringify(news, null, 2);
+    await fs.promises.writeFile(`news_page.json`, jsonData, 'utf8');
+  } catch (err) {
+    console.error("파일 저장 실패", err);
+  }
 }
+
+getNewsInfo();
+
 
 
