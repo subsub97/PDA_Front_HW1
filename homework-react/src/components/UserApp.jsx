9d17d0d916a8c450337330f891edaf78322f2c4e
Ho-Da-Dak <godqhr721@gmail.com>
2024-05-17 19:37:39 +0900

    feature : UserApp.jsx 유저 정보 테이블 UI 구현

--- homework-react/src/components/UserApp.jsx
+++ homework-react/src/components/UserApp.jsx
@@ -1,13 +1,40 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import axios from "axios";
 import ListGroup from "react-bootstrap/ListGroup";
 import { Button, Col, Container, Modal, Row } from "react-bootstrap";
 
 export default function UserApp() {
+  const [user, setUser] = useState([]);
+
+  useEffect(() => {
+    async function getUser() {
+      try {
+        const response = await axios.get("http://jsonplaceholder.typicode.com/users");
+        setUser(response.data);
+        console.log(response.data);
+      } catch (error) {
+        console.error('데이터를 불러오는 데 실패', error);
+      }
+    }
+    getUser();
+  },[]);
+
   return (
     <div>
       <h1>Users</h1>
+      {UserListGroup(user)}
     </div>
   )
+}
+
+function UserListGroup(user) {
+  return (
+    <ListGroup>
+      {user.map((userInfo, index) => (
+        <ListGroup.Item key={index}>{`${index+1}.${userInfo.name} - ${userInfo.email}`}</ListGroup.Item>
+      ))}
+
+    </ListGroup>
+  );
 }
 
